ba3a4ed095f5299d47b7ebf1074c01614e66ece0
Nikolai Damm <nikolai.damm@gmail.com>
2018-04-04 20:22:05 +0200

    Fixed Glide

--- app/src/main/java/com/example/daniel/movieapp/Fragments/DetailsFragment.java
+++ app/src/main/java/com/example/daniel/movieapp/Fragments/DetailsFragment.java
@@ -1,15 +1,12 @@
 package com.example.daniel.movieapp.Fragments;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -20,8 +17,8 @@ import butterknife.ButterKnife;
 
 public class DetailsFragment extends Fragment {
 
-
-    String internetUrl = "https://image.tmdb.org/t/p/w500/" + getArguments().getString("posterPoth");
+    String posterPath;
+    String internetUrl;
 
     TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
 
@@ -38,6 +35,8 @@ public class DetailsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
@@ -45,10 +44,14 @@ public class DetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_details, container, false);
+        posterPath = getArguments().getString("imageView");
+        internetUrl = "https://image.tmdb.org/t/p/w500" + posterPath;
 
-        Glide.with(getContext()).load(internetUrl).into(imageView);
-        title.setText(getArguments().getString("nameOfMovie"));
+
+
         ButterKnife.bind(this, v);
+        title.setText(getArguments().getString("nameOfMovie"));
+        Glide.with(getContext()).load(internetUrl).into(imageView);
         return v;
     }
 
